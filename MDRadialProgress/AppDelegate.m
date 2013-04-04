91661a6428b00df2d7e3a8501db96f5009b9ad78
Marco Dinacci <marco.dinacci@gmail.com>
2013-04-04 09:24:39 +0100

    Removed xib file

--- MDRadialProgress/AppDelegate.m
+++ MDRadialProgress/AppDelegate.m
@@ -16,7 +16,7 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    self.viewController = [[ViewController alloc] initWithNibName:nil bundle:nil];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
