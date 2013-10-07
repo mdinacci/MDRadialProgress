a7e74d063cb26f3aa742f91bb0a436616c0aa141
Marco Dinacci <marco.dinacci@gmail.com>
2013-10-07 11:19:59 +0100

    Initial import of MDRadialProgressTheme files.

---
+++ MDRadialProgress/MDRadialProgressTheme.h
@@ -0,0 +1,13 @@
+//
+//  MDRadialProgressTheme.h
+//  MDRadialProgress
+//
+//  Created by Marco Dinacci on 07/10/2013.
+//  Copyright (c) 2013 Marco Dinacci. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MDRadialProgressTheme : NSObject
+
+@end
---
+++ MDRadialProgress/MDRadialProgressTheme.m
@@ -0,0 +1,13 @@
+//
+//  MDRadialProgressTheme.m
+//  MDRadialProgress
+//
+//  Created by Marco Dinacci on 07/10/2013.
+//  Copyright (c) 2013 Marco Dinacci. All rights reserved.
+//
+
+#import "MDRadialProgressTheme.h"
+
+@implementation MDRadialProgressTheme
+
+@end
