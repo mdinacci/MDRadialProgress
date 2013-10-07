85c0662ef05dd6c8e7c6cf374a6d8037e374ead4
Marco Dinacci <marco.dinacci@gmail.com>
2013-10-07 10:58:43 +0100

    Initial import, unused for the moment.

---
+++ MDRadialProgress/MDRadialProgressLabel.m
@@ -0,0 +1,31 @@
+//
+//  MDRadialProgressLabel.m
+//  MDRadialProgress
+//
+//  Created by Marco Dinacci on 07/10/2013.
+//  Copyright (c) 2013 Marco Dinacci. All rights reserved.
+//
+
+#import "MDRadialProgressLabel.h"
+
+@implementation MDRadialProgressLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
