1004cc55b9bd3274a7784fe2e2a02223546f7d7f
Marco Dinacci <marco.dinacci@gmail.com>
2014-05-06 21:14:15 +0100

    Fixed completely messed up ViewController

--- MDRadialProgress/ViewController.m
+++ MDRadialProgress/ViewController.m
@@ -1,303 +1,236 @@
 //
-// MDRadialProgressView.m
+// ViewController.m
 // MDRadialProgress
 //
 //
 // Copyright (c) 2013 Marco Dinacci. All rights reserved.
 
 
-#import <QuartzCore/QuartzCore.h>
+#import "ViewController.h"
 #import "MDRadialProgressView.h"
-#import "MDRadialProgressLabel.h"
 #import "MDRadialProgressTheme.h"
+#import "MDRadialProgressLabel.h"
 
+@implementation ViewController
 
-@interface MDRadialProgressView ()
-
-// Padding from the view bounds to the outer circumference of the view.
-// Useful because at times the circle may appear "cut" by one or two pixels
-// since it's drawing over the view bounds.
-@property (assign, nonatomic) NSUInteger internalPadding;
-
-@end
-
-
-@implementation MDRadialProgressView
-
-#pragma mark - Initialization
-
-- (id)initWithFrame:(CGRect)frame
+- (MDRadialProgressView *)progressViewWithFrame:(CGRect)frame
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-        [self internalInitWithTheme:[MDRadialProgressTheme standardTheme]];
-    }
-    return self;
+	MDRadialProgressView *view = [[MDRadialProgressView alloc] initWithFrame:frame];
+	
+	// Only required in this demo to align vertically the progress views.
+	view.center = CGPointMake(self.view.center.x + 80, view.center.y);
+	
+	return view;
 }
 
-- (id)initWithFrame:(CGRect)frame andTheme:(MDRadialProgressTheme *)theme
+- (UILabel *)labelAtY:(CGFloat)y andText:(NSString *)text
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-        [self internalInitWithTheme:theme];
-    }
-    return self;
+	CGRect frame = CGRectMake(5, y, 180, 50);
+	UILabel *label = [[UILabel alloc] initWithFrame:frame];
+	label.text = text;
+	label.numberOfLines = 0;
+	label.textAlignment = NSTextAlignmentCenter;
+	label.font = [label.font fontWithSize:14];
+	
+	return label;
 }
 
-- (id)initWithCoder:(NSCoder *)decoder
+- (void)viewDidLoad
 {
-	self = [super initWithCoder:decoder];
-    if (self) {
-		[self internalInitWithTheme:[MDRadialProgressTheme standardTheme]];
-    }
+    [super viewDidLoad];
 	
-	return self;
-}
-
-- (void)dealloc
-{
-    [self removeObserver:self.label forKeyPath:keyThickness];
-}
-
-- (void)internalInitWithTheme:(MDRadialProgressTheme *)theme
-{
-    // Default values for public properties
-	self.progressTotal = 1;
-	self.progressCounter = 0;
-	self.startingSlice = 1;
-    self.clockwise = YES;
+	UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.frame];
+	scrollView.contentSize = CGSizeMake(self.view.bounds.size.width, 800);
+	[self.view addSubview:scrollView];
 	
-	// Use standard theme by default
-	self.theme = theme;
-	
-	// Init the progress label, even if not visible.
-	self.label = [[MDRadialProgressLabel alloc] initWithFrame:self.bounds andTheme:self.theme];
-	[self addSubview:self.label];
-	
-	// Private properties
-	self.internalPadding = 2;
-	
-	// Accessibility
-	self.isAccessibilityElement = YES;
-	self.accessibilityLabel = NSLocalizedString(@"Progress", nil);
-	
-	// Important to avoid showing artifacts
-	self.backgroundColor = [UIColor clearColor];
-	
-	// Register the progress label for changes in the thickness so that it can be repositioned.
-	[self addObserver:self.label forKeyPath:keyThickness options:NSKeyValueObservingOptionNew context:nil];
-}
-
-#pragma mark - Setters
-
-- (void)setProgressCounter:(NSUInteger)progressCounter
-{
-	_progressCounter = progressCounter;
-	[self notifyProgressChange];
+	self.view.backgroundColor = [UIColor whiteColor];
     
-    // setNeedsDisplay needs to be in the main queue to update
-	// the drawRect if the caller of this method is in a different queue.
-	dispatch_async(dispatch_get_main_queue(), ^{
-        [self setNeedsDisplay];
-    });
-}
-
-- (void)setProgressTotal:(NSUInteger)progressTotal
-{
-	_progressTotal = progressTotal;
-	[self notifyProgressChange];
+	int x = self.view.center.x + 80;
+	int y = 20;
 	
-	dispatch_async(dispatch_get_main_queue(), ^{
-        [self setNeedsDisplay];
-    });
-}
-
-#pragma mark - Drawing
-
-- (void)drawRect:(CGRect)rect
-{
-    if (self.progressTotal <= 0) {
-        return;
-    }
+	//	Example 1 ========================================================================
+	UILabel *label = [self labelAtY:y andText:@"Standard theme: "];
+	[scrollView addSubview:label];
+	
+    CGRect frame = CGRectMake(x, y, 50, 50);
+    MDRadialProgressView *radialView = [self progressViewWithFrame:frame];
+    radialView.progressTotal = 7;
+    radialView.progressCounter = 4;
+	radialView.theme.sliceDividerHidden = YES;
+    [scrollView addSubview:radialView];
     
-	CGContextRef contextRef = UIGraphicsGetCurrentContext();
-	CGSize viewSize = self.bounds.size;
-	CGPoint center = CGPointMake(viewSize.width / 2, viewSize.height / 2);
+	//	Example 1 ========================================================================
 	
-	// Draw the slices if there's at least some progress or if, even without progress, the slice dividers are visible.
-	if (self.progressCounter > 0 || (self.progressCounter == 0 && ! self.theme.sliceDividerHidden)) {
-		double radius = viewSize.width / 2 - self.internalPadding;
-		[self drawSlices:self.progressTotal
-			   completed:self.progressCounter
-				  radius:radius
-				  center:center
-			   inContext:contextRef];
-	}
+	y += 80;
 	
-	// Draw the slice separators, unless the sliceDividerHidden property is true.
-	if (! self.theme.sliceDividerHidden) {
-		[self drawSlicesSeparators:contextRef withViewSize:viewSize andCenter:center];
-	}
+	//	Example 2 ========================================================================
+	label = [self labelAtY:y andText:@"Custom theme, shared by all instances: "];
+	[scrollView addSubview:label];
 	
-    // Draw the center.
-	[self drawCenter:contextRef withViewSize:viewSize andCenter:center];
-}
-
-- (void)drawSlices:(NSUInteger)slicesCount
-         completed:(NSUInteger)slicesCompleted
-            radius:(double)circleRadius
-            center:(CGPoint)center
-         inContext:(CGContextRef)context
-{
-    BOOL cgClockwise = !self.clockwise;
-    NSUInteger startingSlice = self.startingSlice -1;
+	MDRadialProgressTheme *newTheme = [[MDRadialProgressTheme alloc] init];
+	newTheme.completedColor = [UIColor colorWithRed:90/255.0 green:212/255.0 blue:39/255.0 alpha:1.0];
+	newTheme.incompletedColor = [UIColor colorWithRed:164/255.0 green:231/255.0 blue:134/255.0 alpha:1.0];
+	newTheme.centerColor = [UIColor clearColor];
+	newTheme.centerColor = [UIColor colorWithRed:224/255.0 green:248/255.0 blue:216/255.0 alpha:1.0];
+	newTheme.sliceDividerHidden = YES;
+	newTheme.labelColor = [UIColor blackColor];
+	newTheme.labelShadowColor = [UIColor whiteColor];
+	
+	y += 50;
+	
+	frame = CGRectMake(10, y, 60, 60);
+    MDRadialProgressView *radialView7 = [[MDRadialProgressView alloc] initWithFrame:frame andTheme:newTheme];
+	radialView7.progressTotal = 4;
+	radialView7.progressCounter = 1;
+	[scrollView addSubview:radialView7];
+	
+	frame = CGRectMake(90, y, 60, 60);
+	MDRadialProgressView *radialView8 = [[MDRadialProgressView alloc] initWithFrame:frame andTheme:newTheme];
+	radialView8.progressTotal = 4;
+	radialView8.progressCounter = 2;
+	[scrollView addSubview:radialView8];
+	
+	frame = CGRectMake(170, y, 60, 60);
+	MDRadialProgressView *radialView9 = [[MDRadialProgressView alloc] initWithFrame:frame andTheme:newTheme];
+	radialView9.progressTotal = 4;
+	radialView9.progressCounter = 3;
+	[scrollView addSubview:radialView9];
+	
+	frame = CGRectMake(250, y, 60, 60);
+	MDRadialProgressView *radialView10 = [[MDRadialProgressView alloc] initWithFrame:frame andTheme:newTheme];
+	radialView10.progressTotal = 4;
+	radialView10.progressCounter = 4;
+	[scrollView addSubview:radialView10];
+	//	Example 2 ========================================================================
+	
+	y += 100;
+	
+	//	Example 3 ========================================================================
+	label = [self labelAtY:y andText:@"incompletedColor is clear, no divider, no label:"];
+	[scrollView addSubview:label];
+	
+	frame = CGRectMake(x, y, 100, 100);
+    MDRadialProgressView *radialView2 = [self progressViewWithFrame:frame];
+	radialView2.progressTotal = 5;
+    radialView2.progressCounter = 3;
+    radialView2.theme.thickness = 15;
+	radialView2.theme.incompletedColor = [UIColor clearColor];
+	radialView2.theme.completedColor = [UIColor orangeColor];
+    radialView2.theme.sliceDividerHidden = YES;
+	radialView2.label.hidden = YES;
+	
+	[scrollView addSubview:radialView2];
     
-	if (! self.theme.sliceDividerHidden && self.theme.sliceDividerThickness > 0) {
-		// Draw one arc at a time.
-        
-        double sliceAngle = (2 * M_PI ) / slicesCount;
-        for (int i =0; i < slicesCount; i++) {
-            double startValue = (sliceAngle * i) + sliceAngle * startingSlice;
-            double startAngle, endAngle;
-            if (self.clockwise) {
-                startAngle = - M_PI_2 + startValue;
-				endAngle = startAngle + sliceAngle;
-            } else {
-                startAngle = - M_PI_2 - startValue;
-				endAngle = startAngle - sliceAngle;
-            }
-            
-            CGContextBeginPath(context);
-            CGContextMoveToPoint(context, center.x, center.y);
-            CGContextAddArc(context, center.x, center.y, circleRadius,
-							startAngle, endAngle, cgClockwise);
-            
-            CGColorRef color = self.theme.incompletedColor.CGColor;
-            
-            if (i < slicesCompleted) {
-                color = self.theme.completedColor.CGColor;
-            }
-            CGContextSetFillColorWithColor(context, color);
-            CGContextFillPath(context);
-        }
-    } else {
-		// Draw just two arcs, one for the completed slices and one for the
-		// uncompleted ones.
-		
-        double originAngle, endAngle;
-		double sliceAngle = (2 * M_PI) / self.progressTotal;
-		double startingAngle = sliceAngle * startingSlice;
-		double progressAngle = sliceAngle * self.progressCounter;
-		
-		if (self.progressCounter == 0) {
-			originAngle = -M_PI_2;
-			endAngle = originAngle + 2 * M_PI;
-		} else {
-			if (self.clockwise) {
-				originAngle = -M_PI_2 + startingAngle;
-				endAngle = originAngle + progressAngle;
-			} else {
-				originAngle = -M_PI_2 - startingAngle;
-				endAngle = originAngle - progressAngle;
-			}
-		}
-		
-		// Draw the arcs grouped instead of individually to avoid
-		// artifacts between one slice and another.
-		
-		// Completed slices.
-		CGContextBeginPath(context);
-		CGContextMoveToPoint(context, center.x, center.y);
-		CGContextAddArc(context, center.x, center.y, circleRadius, originAngle, endAngle, cgClockwise);
-		CGColorRef color = self.theme.completedColor.CGColor;
-		CGContextSetFillColorWithColor(context, color);
-		CGContextFillPath(context);
-		
-		if (self.progressCounter < self.progressTotal) {
-			// Incompleted slices
-			CGContextBeginPath(context);
-			CGContextMoveToPoint(context, center.x, center.y);
-			double startAngle = endAngle;
-			CGContextAddArc(context, center.x, center.y, circleRadius, startAngle, originAngle, cgClockwise);
-			color = self.theme.incompletedColor.CGColor;
-			CGContextSetFillColorWithColor(context, color);
-			CGContextFillPath(context);
-		}
-	}
+	//	Example 3 ========================================================================
+	
+	y += 130;
+	
+	//	Example 4 ========================================================================
+	label = [self labelAtY:y andText:@"Center color = completed color, no divider: "];
+	[scrollView addSubview:label];
+	
+	frame = CGRectMake(x, y, 100, 100);
+    MDRadialProgressView *radialView3 = [self progressViewWithFrame:frame];
+	
+	radialView3.progressTotal = 5;
+    radialView3.progressCounter = 4;
+	radialView3.theme.completedColor = [UIColor colorWithRed:247/255.0 green:247/255.0 blue:247/255.0 alpha:1.0];
+	radialView3.theme.incompletedColor = [UIColor blackColor];
+    radialView3.theme.thickness = 10;
+    radialView3.theme.sliceDividerHidden = YES;
+	radialView3.theme.centerColor = radialView3.theme.completedColor;
+	
+	[scrollView addSubview:radialView3];
+	//	Example 4 ========================================================================
+	
+	y += 130;
+	
+	//	Example 5 ========================================================================
+	label = [self labelAtY:y andText:@"No label, anti-clockwise: "];
+	[scrollView addSubview:label];
+	
+	frame = CGRectMake(x, y, 70, 70);
+    MDRadialProgressView *radialView4 = [self progressViewWithFrame:frame];
+    
+	radialView4.progressTotal = 10;
+    radialView4.progressCounter = 3;
+	radialView4.clockwise = NO;
+	radialView4.theme.completedColor = [UIColor colorWithRed:90/255.0 green:200/255.0 blue:251/255.0 alpha:1.0];
+	radialView4.theme.incompletedColor = [UIColor colorWithRed:82/255.0 green:237/255.0 blue:199/255.0 alpha:1.0];
+    radialView4.theme.thickness = 30;
+    radialView4.theme.sliceDividerHidden = NO;
+    radialView4.theme.sliceDividerColor = [UIColor whiteColor];
+    radialView4.theme.sliceDividerThickness = 2;
+	radialView4.label.hidden = YES;
+    
+    [scrollView addSubview:radialView4];
+	
+	//	Example 5 ========================================================================
+	
+	y += 110;
+	
+	//	Example 6 ========================================================================
+	label = [self labelAtY:y andText:@"Start from slice 3, custom label color with no shadow: "];
+	[scrollView addSubview:label];
+	
+    frame = CGRectMake(x, y, 100, 100);
+    MDRadialProgressView *radialView5 = [self progressViewWithFrame:frame];
+	
+	radialView5.progressTotal = 10;
+    radialView5.progressCounter = 3;
+	radialView5.startingSlice = 3;
+    radialView5.theme.sliceDividerHidden = NO;
+    radialView5.theme.sliceDividerThickness = 1;
+	radialView5.label.textColor = [UIColor blueColor];
+	radialView5.label.shadowColor = [UIColor clearColor];
+	
+	[scrollView addSubview:radialView5];
+	//	Example 6 ========================================================================
+	
+	y += 130;
+	
+	//	Example 7 ========================================================================
+	label = [self labelAtY:y andText:@"Huge thickness, no divider: "];
+	[scrollView addSubview:label];
+	
+    frame = CGRectMake(x, y, 100, 100);
+    MDRadialProgressView *radialView6 = [self progressViewWithFrame:frame];
+	
+	radialView6.startingSlice = 5;
+	radialView6.progressTotal = 20;
+    radialView6.progressCounter = 9;
+	radialView6.startingSlice = 3;
+	radialView6.clockwise = YES;
+    radialView6.theme.thickness = 70;
+	radialView6.theme.completedColor = [UIColor brownColor];
+	radialView6.theme.sliceDividerThickness = 0;
+    
+	[scrollView addSubview:radialView6];
+	//	Example 7 ========================================================================
+	
+	y += 130;
+	
+	//	Example 8 ========================================================================
+	label = [self labelAtY:y andText:@"No progress, show dividers anyway"];
+	[scrollView addSubview:label];
+	
+	frame = CGRectMake(x, y, 100, 100);
+    MDRadialProgressView *radialView11 = [self progressViewWithFrame:frame];
+	radialView11.progressTotal = 10;
+    radialView11.progressCounter = 0;
+	radialView11.theme.incompletedColor = [UIColor clearColor];
+	radialView11.theme.centerColor = [UIColor whiteColor];
+	radialView11.theme.sliceDividerColor = [UIColor grayColor];
+	radialView11.theme.sliceDividerThickness = 2;
+	radialView11.theme.sliceDividerHidden = NO;
+	radialView11.label.hidden = YES;
+	[scrollView addSubview:radialView11];
 }
 
-- (void)drawSlicesSeparators:(CGContextRef)contextRef withViewSize:(CGSize)viewSize andCenter:(CGPoint)center
+- (void)didReceiveMemoryWarning
 {
-	int outerDiameter = viewSize.width;
-    double outerRadius = outerDiameter / 2.0 - self.internalPadding;
-    int innerDiameter = (outerDiameter - self.theme.thickness);
-    double innerRadius = innerDiameter / 2.0;
-	int sliceCount = self.progressTotal;
-	double sliceAngle = (2 * M_PI) / sliceCount;
-	
-	CGContextSetLineWidth(contextRef, self.theme.sliceDividerThickness);
-	CGContextSetStrokeColorWithColor(contextRef, self.theme.sliceDividerColor.CGColor);
-	CGContextMoveToPoint(contextRef, center.x, center.y);
-	
-	for (int i = 0; i < sliceCount; i++) {
-		CGContextBeginPath(contextRef);
-		
-		double startAngle = sliceAngle * i - M_PI_2;
-		double endAngle = sliceAngle * (i + 1) - M_PI_2;
-		
-		// Draw the outer slice arc clockwise.
-		CGContextAddArc(contextRef, center.x, center.y, outerRadius, startAngle, endAngle, 0);
-		// Draw the inner slice arc in the opposite direction. The separator line is drawn automatically when moving
-		// from the point where the outer arc ended to the point where the inner arc starts.
-		CGContextAddArc(contextRef, center.x, center.y, innerRadius, endAngle, startAngle, 1);
-		CGContextStrokePath(contextRef);
-	}
-}
-
-- (void)drawCenter:(CGContextRef)contextRef withViewSize:(CGSize)viewSize andCenter:(CGPoint)center
-{
-	int innerDiameter = viewSize.width - self.theme.thickness;
-    double innerRadius = innerDiameter / 2.0;
-	
-	CGContextSetLineWidth(contextRef, self.theme.thickness);
-	CGRect innerCircle = CGRectMake(center.x - innerRadius, center.y - innerRadius,
-									innerDiameter, innerDiameter);
-	CGContextAddEllipseInRect(contextRef, innerCircle);
-	CGContextClip(contextRef);
-	CGContextClearRect(contextRef, innerCircle);
-	CGContextSetFillColorWithColor(contextRef, self.theme.centerColor.CGColor);
-	CGContextFillRect(contextRef, innerCircle);
-}
-
-# pragma mark - Accessibility
-
-- (UIAccessibilityTraits)accessibilityTraits
-{
-	return [super accessibilityTraits] | UIAccessibilityTraitUpdatesFrequently;
-}
-
-# pragma mark - Notifications
-
-- (void)notifyProgressChange
-{
-	// Update the accessibilityValue and the progressSummaryView text.
-	
-    NSString *text;
-	
-    if (self.labelTextBlock) {
-        text = self.labelTextBlock(self);
-    } else {
-        float percentageCompleted = (100.0f / self.progressTotal) * self.progressCounter;
-        text = [NSString stringWithFormat:@"%.0f", percentageCompleted];
-    }
-	
-	self.accessibilityValue = text;
-	self.label.text = text;
-	
-	NSString *notificationText = [NSString stringWithFormat:@"%@ %@",
-								  NSLocalizedString(@"Progress changed to:", nil),
-								  self.accessibilityValue];
-	UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification, notificationText);
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 @end
